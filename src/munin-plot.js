28b69591f05d4e08faf576a38b7feb420f1893d3
Arthur de Jong <arthur@arthurdejong.org>
2021-12-12 18:33:24 +0100

    First attempt at providing a print style
    Whether this actually works is heavily dependant on the browser and in
    some cases the screen size. Support in Plotly is not completely there
    and there are also various bugs in browsers, e.g.:
    
    https://github.com/plotly/plotly.js/issues/1275
    https://bugs.chromium.org/p/chromium/issues/detail?id=697233

--- src/munin-plot.js
+++ src/munin-plot.js
@@ -879,4 +879,31 @@ $(document).ready(function () {
   window.addEventListener('hashchange', function () {
     loadDashboardFromHash()
   })
+
+  function resizeForPrinting() {
+    $('.myplot').each(function () {
+      if (this.layout) {
+        const box = this.getBoundingClientRect()
+        Plotly.relayout(this, {width: box.width, height: box.height})
+      }
+    })
+  }
+
+  function restoreFromPrinting() {
+    $('.myplot').each(function () {
+      if (this.layout) {
+        Plotly.relayout(this, {width: null, height: null, autosize: true})
+      }
+    })
+  }
+
+  window.addEventListener('beforeprint', resizeForPrinting)
+  window.addEventListener('afterprint', restoreFromPrinting)
+  window.matchMedia('print').addListener(function (mql) {
+    if (mql.matches) {
+      resizeForPrinting()
+    } else {
+      restoreFromPrinting()
+    }
+  })
 })
